9964ae285e4b579389dc05e3c1ee1ecde6202b29
Ryan Burton <rryanburton@gmail.com>
2015-09-02 21:04:01 -0400

    lots of changes trying to make it work

--- palindrome.py
+++ palindrome.py
@@ -1,26 +1,39 @@
-def is_palindrome(sentence): # TODO: return True or False if the sentence is or isn't a palindrome
-    #if len(sentence) = 0 or = 1 then it's TRUE a palindrome (length <= to 1)
+def is_palindrome(sentence):
+    # TODO: return True or False if the sentence is or isn't a palindrome
+    ##if len(sentence) = 0 or = 1 then it's TRUE a palindrome (length <= to 1)
+    alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    sentence = []
+    for letter in user_input.lower():
+        if letter in alphabet_list:
+            sentence.append(letter)
     if len(sentence) <= 1:
-        return TRUE
-    
-alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-user_input= "test $sentence with punctuation, !$"
+        return True
+
+    #now i need to deal with phrases longer than 1 letter in length
+    elif (is_palindrome(sentence[1:-1]) and sentence[0] == sentence[-1]):
+        return True
+
+    else:
+        return False
 
 
-# include characters in input that are also in alphabet_list, but exclude everything not in alphabet_list
-sentence = []
-for letter in user_input.lower():
-    if letter in alphabet_list:
-        sentence.append(letter)
+
+
+
 #reversed_list = user_letters[::-1]
 
 
 
 
 def main():
-    user_input = input("give me some words to see if they are palindrome") # TODO: put your input/output code here
+    user_input = input("give me some words to see if they are palindrome  ")
 
-
+    is_palindrome(user_input)
+#if True:
+#    print("it is one")
+#if False:
+#    print("it is not one")
 
 if __name__ == '__main__':
-    main()
+   main()
